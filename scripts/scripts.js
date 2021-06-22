e0f804a5b0c5287df8e5203e3057f8de19050caa
Guillermo <guillermoc93@protonmail.com>
2021-06-22 13:11:20 -0500

    tic tac toe grid display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,13 @@
 const gameBoard = (() => {
   "use strict";
 
-  const board = [1, 2, 3, 4, 5, 6, 7, 8];
+  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9;
+})();
+
+const displayController = (() => {
+  "use strict";
+
+  const 
 })();
 
 const playerFactory = (name, symbol) => {
