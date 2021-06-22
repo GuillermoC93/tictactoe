66b1375499e401c229dc10614173cb8995c749ce
Guillermo <guillermoc93@protonmail.com>
2021-06-22 13:46:59 -0500

    hover effect for each cell

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,23 @@
 const gameBoard = (() => {
   "use strict";
 
-  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9;
+  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 })();
 
 const displayController = (() => {
   "use strict";
 
-  const 
+  const grid = document.querySelector(".container")
+
+  const handler = (event) => {
+    if (event.target != grid) {
+      event.target.classList.toggle('hover')
+    }
+  }
+
+  grid.addEventListener("mouseover", handler)
+  grid.addEventListener("mouseout", handler)
+
 })();
 
 const playerFactory = (name, symbol) => {
