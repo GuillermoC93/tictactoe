07fef4032cfe2daaa57ae0461ac800d915d3ed77
Guillermo <guillermoc93@protonmail.com>
2021-06-23 10:43:48 -0500

    displaying with board array contents

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,34 @@
+const playerFactory = (name, symbol) => {
+  const sayHello = () => console.log("Hello!")
+  return { name, symbol, sayHello };
+};
+
 const gameBoard = (() => {
   "use strict";
 
-  const board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+
+  return { board }
 })();
 
 const displayController = (() => {
   "use strict";
 
-  const grid = document.querySelector(".container")
+  const grid = document.querySelector(".container");
+  const cells = document.querySelectorAll('[data-index]');
+
+  cells.forEach((cell, index) => {
+    cell.textContent = gameBoard.board[index];
+    cell.classList.toggle('symbol');
+  });
 
   const handler = (event) => {
     if (event.target != grid) {
       event.target.classList.toggle('hover')
     }
-  }
+  };
 
-  grid.addEventListener("mouseover", handler)
-  grid.addEventListener("mouseout", handler)
-
+  // event handlers for mouseover effect
+  grid.addEventListener("mouseover", handler);
+  grid.addEventListener("mouseout", handler);
 })();
-
-const playerFactory = (name, symbol) => {
-  const sayHello = () => console.log("Hello!")
-  return { name, symbol, sayHello };
-};
