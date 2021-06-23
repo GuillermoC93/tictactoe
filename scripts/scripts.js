57fd2796cb5332dee4e821002f987c85433a8f04
Guillermo <guillermoc93@protonmail.com>
2021-06-23 15:12:28 -0500

    displayMove function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,30 +22,64 @@ const displayController = (() => {
     cell.textContent = gameBoard.board[index];
   })};
 
-  const handler = (event) => {
+  // handler for hover over mouse event
+  const hoverHandler = (event) => {
     if (event.target != grid) {
       event.target.classList.toggle('hover')
     }
   };
 
-  // event handlers for mouseover effect
-  grid.addEventListener("mouseover", handler);
-  grid.addEventListener("mouseout", handler);
-
-  // function for displaying each player move to the onscreen board
-  const displayMove = (player) => {
-    grid.addEventListener("click", event => {
+  // handler for click event
+  const clickHandler = (player) => {
+    return function(event) {
       if (event.target != grid) {
         if (gameBoard.board[event.target.dataset.index] == "") {
           gameBoard.board[event.target.dataset.index] = player.symbol
-          event.target.classList.toggle('symbol')
-          populateDisplay()
+          event.target.classList.toggle('symbol');
+          populateDisplay();
         } else {
           return
         }
       }
-    })
+    }
+  }
+
+  // event handlers for mouseover effect
+  grid.addEventListener("mouseover", hoverHandler);
+  grid.addEventListener("mouseout", hoverHandler);
+
+  // function for displaying each player move to the onscreen board
+  // uses a "once" option to only fire the event listener once
+  const displayMove = (player) => {
+    grid.addEventListener("click", clickHandler(player), { once: true });
   }
 
   return { displayMove }
+
 })();
+
+const gameController = (() => {
+  "use strict";
+
+  let player1 = playerFactory("john", "X");
+  let player2 = playerFactory("amy", "O");
+  let currentPlayer = player1
+
+  const switchPlayer = () => {
+    if (currentPlayer == player1) {
+      currentPlayer = player2 
+    } else {
+      return
+    }
+  }
+
+  const gameStart = function () {
+    displayController.displayMove(currentPlayer);
+    switchPlayer();
+  }
+
+  return { gameStart }
+
+})();
+
+gameController.gameStart();
